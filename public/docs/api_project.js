43a899418270de7a282aa17b93ecc9603a3c6005
xiongjian <webjsforyou@163.com>
2020-06-13 14:43:09 +0800

    docs: 增加文档

---
+++ public/docs/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "title": "硅谷教育管理系统接口文档",
+  "url": "http://47.103.203.152",
+  "name": "硅谷教育管理系统接口文档",
+  "version": "1.0.0",
+  "description": "硅谷教育管理系统接口文档",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2020-06-13T06:42:50.249Z",
+    "url": "http://apidocjs.com",
+    "version": "0.23.0"
+  }
+});
