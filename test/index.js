520a709c3de2b83befbe40d2c11ac4dd40e64ad5
xiongjian <webjsforyou@gmail.com>
2020-06-20 20:10:54 +0800

    fix: 导入数据

--- test/index.js
+++ test/index.js
@@ -3,24 +3,25 @@ const path = require("path");
 const fs = require("fs");
 
 function resolve(relative) {
-  return path.resolve(__dirname, relative);
+	return path.resolve(__dirname, relative);
 }
 
 // 删库
 execSync('mongo react_admin --eval "db.dropDatabase()"');
+// 创建数据库
+execSync("mongo react_admin");
 
 const dirs = fs.readdirSync(resolve("dbs"));
-console.log(dirs);
 
 for (let i = 0; i < dirs.length; i++) {
-  const dir = dirs[i];
+	const dir = dirs[i];
 
-  const name = dir.split(".")[1];
+	const name = dir.split(".")[1];
 
-  // 导入基本数据
-  execSync(
-    `mongoimport --db react_admin --collection ${name} --type json --file ${resolve(
-      `dbs/${dir}`
-    )}`
-  );
+	// 导入基本数据
+	execSync(
+		`mongoimport --db react_admin --collection ${name} --type json --file ${resolve(
+			`dbs/${dir}`
+		)}`
+	);
 }
